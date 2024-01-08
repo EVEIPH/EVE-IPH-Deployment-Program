8272ecff48f1c345e5dc186e68daff2fc8ab1493
Zifrian <ZifrianEVE@gmail.com>
2024-01-07 22:18:59 -0500

    Update frmMain.Designer.vb

--- frmMain.Designer.vb
+++ frmMain.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class frmMain
+Partial Class FrmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
